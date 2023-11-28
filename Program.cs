faacce64c5b7e000b1929f4f6882044310ca9da6
Mophat <bezylmophatotieno@gmail.com>
2023-11-28 08:53:35 +0300

    Made changes to allow the user to switch between Either Login or Register

--- Program.cs
+++ Program.cs
@@ -4,18 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Console.WriteLine("Choose option 1 to Register 2 to Login \n");
+        string option = Console.ReadLine();
+
+        if(option == "1"){
         // Directory.CreateDirectory(@"D:\Teach2Give\assignments\Account\Registrations");
         // File.Create(@"D:\Teach2Give\assignments\Account\Registrations\data1.txt");
-        // string path = @"D:\Teach2Give\assignments\Account\Registrations\data1.txt";
-        // Console.WriteLine("Welcome to the registration system \n");
-        // Console.WriteLine("Enter Your Username");
-        // string username = Console.ReadLine();
-        // Console.WriteLine("Enter Your Password");
-        // string password = Console.ReadLine();
-        // // Register the User
-        // Action.Registraton(username,password,path);
+        string path = @"D:\Teach2Give\assignments\Account\Registrations\data1.txt";
+        Console.WriteLine("Welcome to the registration system \n");
+        Console.WriteLine("Enter Your Username");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter Your Password");
+        string password = Console.ReadLine();
+        // Register the User
+        Action.Registraton(username,password,path);
 
-        // Login the user 
+        }else if(option == "2"){
+
+                    // Login the user 
         bool loginStatus = false;
         do {
          string path = @"D:\Teach2Give\assignments\Account\Registrations\data1.txt";
@@ -39,5 +46,8 @@ class Program
         }while(!loginStatus);
 
 
+        }else{
+            Console.WriteLine("Invalid Option");
+        }
     }
 }
